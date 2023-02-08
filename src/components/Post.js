419bf41842e97cd200f80b9cb84b77156bc3557c
huu.pb <banghuund99@gmail.com>
2023-02-08 13:41:40 +0700

    add / to link:

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,7 +3,7 @@ function Post({post}) {
     <h2 className="text-2xl font-bold mb-4">{post.title}</h2>
     <p className="text-gray-700">Post published on  <span className="font-medium">{post.author.name}</span></p>
     <img className="w-full mt-4" src={post.coverImage.url} alt="Post Cover"/>
-    {/* <div className="mt-4 text-lg text-gray-700" dangerouslySetInnerHTML={{__html: post.content.html}}></div> */}
+    <div className="mt-4 text-lg text-gray-700" dangerouslySetInnerHTML={{__html: post.content.html}}></div>
   </main> );
 }
 
